2434d3f5f20e33b0ae1300a0f346242bc24b7544
sushant <sushantmankar2@gmail.com>
2025-07-16 15:30:51 +0530

    fghjkl;'

--- day-9-module-calling/main.tf
+++ day-9-module-calling/main.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
 module "project" {
-  source         = "../day-9-modules"
+  source         = "git::https://github.com/sushantmankar12/terraform-practice-01.git//day-9-modules"
   ami_id         = "ami-00399ec92321828f5"
   instance_type  = "t2.micro"
-  key            = ""                     # now it's okay
+  key            = ""  # empty if no key pair
   az             = "us-east-2a"
-  bucket         = "sushant1234"
+  bucket = "sushanth2023"
 }
