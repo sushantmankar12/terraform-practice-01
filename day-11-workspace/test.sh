94cffd3238eb3ce68de04bb06599debc13e27775
sushant <sushantmankar2@gmail.com>
2025-07-18 16:21:42 +0530

    new changes

--- day-11-workspace/test.sh
+++ day-11-workspace/test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-echo "Hello from $(hostname)" > /var/www/html/index.html
+
+# Update the package list
+apt update -y
+
+# Install Apache2
+apt install apache2 -y
+
+# Start the Apache2 service
+systemctl start apache2
+
+# Enable Apache2 to start on boot
+systemctl enable apache2
+
+# Add custom web page
+echo "<h1>Hello from $(hostname)</h1>" > /var/www/html/index.html
