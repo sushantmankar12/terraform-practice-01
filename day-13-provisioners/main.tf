aec386cb7b0f7e46f28d638a22ff689dd099e50e
sushant <sushantmankar2@gmail.com>
2025-07-22 15:29:46 +0530

    day-13-provisioners files

---
+++ day-13-provisioners/main.tf
@@ -0,0 +1,122 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Key Pair (uses your local public key)
+resource "aws_key_pair" "example" {
+  key_name   = "task"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# VPC
+resource "aws_vpc" "myvpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "sub1" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.myvpc.id
+}
+
+# Route Table and Association
+resource "aws_route_table" "RT" {
+  vpc_id = aws_vpc.myvpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.sub1.id
+  route_table_id = aws_route_table.RT.id
+}
+
+# Security Group
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance (Ubuntu Linux)
+resource "aws_instance" "server" {
+  ami                         = "ami-0b32d400456908bf9" # Ubuntu 22.04 in ap-south-1
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.example.key_name
+  subnet_id                   = aws_subnet.sub1.id
+  vpc_security_group_ids      = [aws_security_group.webSg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "ec2ServerMumbai"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "file-10"
+    destination = "/home/ec2-user/file-10"
+  }
+}
+
+# Remote provisioner to create file200
+resource "null_resource" "run_script" {
+  provisioner "remote-exec" {
+    connection {
+      host        = aws_instance.server.public_ip
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+    }
+
+    inline = [
+      "echo 'hello from awsdevops' > /home/ec2-user/file200"
+    ]
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
